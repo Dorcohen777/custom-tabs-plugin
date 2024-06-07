1583acaee5e1560e4e4e616f3158a5538ee39e77
Dorcohen777 <dorcohen1337@gmail.com>
2024-06-07 12:51:33 +0300

    building the function that register the settings - custom_tabs_settings_init

--- wp-content/plugins/custom-tabs-plugin/custom-tabs-plugin.php
+++ wp-content/plugins/custom-tabs-plugin/custom-tabs-plugin.php
@@ -1,32 +1,41 @@
-<?php 
+<?php
 /*
  * Plugin Name: custom-tabs-plugin
  * Author: Dor cohen
  */
 
- // hook to add admin menu
- add_action('admin_menu', 'custom_tabs_add_admin_menu');
+// Hook to add admin menu
+add_action('admin_menu', 'custom_tabs_add_admin_menu');
 
-// hook to register settings
- add_action('admin_init', 'custom_tabs_settings_init');
+// Hook to register settings
+add_action('admin_init', 'custom_tabs_settings_init');
 
- function custom_tabs_add_admin_menu() {
+// Function to add the options page
+function custom_tabs_add_admin_menu()
+{
     add_options_page(
-        'Custom Tabs Plugin', // Page title
-        'Custom Tabs',        // Menu title
-        'manage_options',     // Capability required
-        'custom-tabs-plugin', // Menu slug
-        'custom_tabs_options_page' // Function to display the page content
-    )
- };
+        'Custom Tabs Plugin',       // Page title
+        'Custom Tabs',              // Menu title
+        'manage_options',           // Capability required
+        'custom-tabs-plugin',       // Menu slug
+        'custom_tabs_options_page'  // Function to display the page content
+    );
+}
 
- function activate_tabs_plugin() {
-    
- }
+// Function to register settings
+function custom_tabs_settings_init() {
+    register_setting('custom_tabs_plugin', 'custom_tabs_options');
+    add_settings_section(
+        'custom_tabs_plugin_section', // Section ID
+        __('Custom Tabs Settings', 'custom-tabs-plugin'), // Title
+        'custom_tabs_settings_section_callback', // Callback to display description
+        'custom_tabs_plugin' // Page slug
+    );
+}
 
 
- // register activation hook
- register_activation_hook(__FILE__, 'activate_tabs_plugin')
+// register activation hook
+register_activation_hook(__FILE__, 'activate_tabs_plugin')
 
 
-?>
+    ?>
