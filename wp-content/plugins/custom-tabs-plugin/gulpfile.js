6222559c46347198b7ef00b0c608cc499e14ac8c
Dorcohen777 <dorcohen1337@gmail.com>
2024-06-07 19:10:34 +0300

    created gulpfile that handle sass complie also added callback function to fix bug, also installed sass/node-sass/gulp-sass

--- wp-content/plugins/custom-tabs-plugin/gulpfile.js
+++ wp-content/plugins/custom-tabs-plugin/gulpfile.js
@@ -1,14 +1,18 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 
-function compileSass() {
-    return gulp.src('scss/**/*.scss')
+function compileSass(done) {
+    gulp.src('scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('css'))
+        .on('end', done); // Signal async completion
 }
 
 function watchFiles() {
     gulp.watch('scss/**/*.scss', compileSass);
 }
+
+exports.build = compileSass;
+exports.watch = watchFiles;
 
 exports.default = gulp.series(compileSass, watchFiles);
