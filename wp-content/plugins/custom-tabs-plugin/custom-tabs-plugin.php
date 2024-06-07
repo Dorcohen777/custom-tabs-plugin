b78a4ab3e5ae69450d902acb17d0eed4ae969830
Dorcohen777 <dorcohen1337@gmail.com>
2024-06-07 10:40:24 +0300

    created register activation hook for plugin and empty function

--- wp-content/plugins/custom-tabs-plugin/custom-tabs-plugin.php
+++ wp-content/plugins/custom-tabs-plugin/custom-tabs-plugin.php
@@ -1,4 +1,16 @@
+<?php 
 /*
  * Plugin Name: custom-tabs-plugin
  * Author: Dor cohen
  */
+
+ function activate_tabs_plugin() {
+    
+ }
+
+
+ // register activation hook
+ register_activation_hook(__FILE__, 'activate_tabs_plugin')
+
+
+?>
