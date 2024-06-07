e89c3ce9540e5cc8915459f623e4dda0f43a4344
Dorcohen777 <dorcohen1337@gmail.com>
2024-06-07 13:06:16 +0300

    added render function that render textarea for the brands title

--- wp-content/plugins/custom-tabs-plugin/custom-tabs-plugin.php
+++ wp-content/plugins/custom-tabs-plugin/custom-tabs-plugin.php
@@ -25,14 +25,34 @@ function custom_tabs_add_admin_menu()
 // Function to register settings
 function custom_tabs_settings_init() {
     register_setting('custom_tabs_plugin', 'custom_tabs_options');
+
     add_settings_section(
         'custom_tabs_plugin_section', // Section ID
         __('Custom Tabs Settings', 'custom-tabs-plugin'), // Title
         'custom_tabs_settings_section_callback', // Callback to display description
         'custom_tabs_plugin' // Page slug
     );
+
+    add_settings_field(
+        'custom_tabs_field_brands_name', // Field ID 
+        __('Case Study Titles', 'custom-tabs-plugin'), // Case Study Titles
+        'custom_tabs_field_brands_name_render', // Callback to render the field
+        'custom_tabs_plugin', // Page Slug
+        'custom_tabs_plugin_section', // Section ID 
+    );
 }
 
+function custom_tabs_field_brands_name_render() {
+    $options = get_option('custom_tabs_options');
+    $titles = isset($options['custom_tabs_field_brands_name']) ? $options['custom_tabs_field_brands_name'] : '';
+
+    ?>
+    <textarea name="custom_tabs_options[custom_tabs_field_brands_name]" rows="5" cols="50"><?php echo esc_textarea($titles); ?></textarea>
+    <p class="description"><?php _e('Enter each title on a new line.', 'custom-tabs-plugin'); ?></p>
+    <?php
+
+    
+}
 
 // register activation hook
 register_activation_hook(__FILE__, 'activate_tabs_plugin')
