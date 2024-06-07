9cca07e38d2a4ad712fb33062d9e3cb9beea35ba
Dorcohen777 <dorcohen1337@gmail.com>
2024-06-07 11:59:52 +0300

    added function custom_tabs_add_admin_menu that - adds new options page

--- wp-content/plugins/custom-tabs-plugin/custom-tabs-plugin.php
+++ wp-content/plugins/custom-tabs-plugin/custom-tabs-plugin.php
@@ -4,6 +4,22 @@
  * Author: Dor cohen
  */
 
+ // hook to add admin menu
+ add_action('admin_menu', 'custom_tabs_add_admin_menu');
+
+// hook to register settings
+ add_action('admin_init', 'custom_tabs_settings_init');
+
+ function custom_tabs_add_admin_menu() {
+    add_options_page(
+        'Custom Tabs Plugin', // Page title
+        'Custom Tabs',        // Menu title
+        'manage_options',     // Capability required
+        'custom-tabs-plugin', // Menu slug
+        'custom_tabs_options_page' // Function to display the page content
+    )
+ };
+
  function activate_tabs_plugin() {
     
  }
